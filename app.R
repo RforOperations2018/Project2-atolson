7cba613f2c10c45ee7e734a387dd43a87cd69e8a
arcanaknight1 <33413009+arcanaknight1@users.noreply.github.com>
2018-10-16 20:49:56 -0400

    Constructed Basic URL query and Date input

--- app.R
+++ app.R
@@ -21,7 +21,8 @@ ckanSQL <- function(url) {
 }
 
 #Building API query with date filter
-#url <-paste0("http://data.wprdc.org/api/3/action/datastore_search_sql?sql=SELECT%20%22Date%22%2C%20%22Gender%22%2C%20%22Race%22%2C%20%22Age%20at%20Booking%22%20AS%20%22AgeatBooking%22%20FROM%20%2266cdcd57-6c92-4aaa-8800-0ed9d8f03e22%22%20WHERE%20%22Date%22%20%3E=%20%27", input$DateSelect[1], "%27%20AND%20%22Date%22%20%3C=%20%27", input$DateSelect[2], "%27")
+url <- paste0("https://data.wprdc.org/api/3/action/datastore_search_sql?sql=SELECT%20%22AGE%22%2C%20%22RACE%22%2C%20%22GENDER%22%2C%20%22ARRESTDATE%22%2C%20%22ARRESTLOCATION%22%2C%20%22OFFENSES%22%2C%20%22INCIDENTLOCATION%22%20FROM%20%22e03a89dd-134a-4ee8-a2bd-62c40aeebc6f%22%20WHERE%20%22Date%22%20%3E=%20%27", input$DateSelect[1], "%27%20AND%20%22Date%22%20%3C=%20%27", input$DateSelect[2], "%27")
+# Variables (nottetaking purposes): AGE, RACE, GENDER, ARRESTDATE, ARRESTLOCATION, OFFENSES, INCIDENTLOCATION
 
 #load and clean data
 #data.load <- ckanSQL(url) %>%
@@ -38,6 +39,13 @@ ui <- fluidPage(
   # Sidebar
   sidebarLayout(
     sidebarPanel(
+      
+      #Date Selecter
+      dateRangeInput("DateSelect",
+                     "Date range:",
+                     start = Sys.Date()-30,
+                     end = Sys.Date(),
+                     format = "mm/dd/yyyy"),
       
       #Neighborhood select
       selectInput("input1Select",
